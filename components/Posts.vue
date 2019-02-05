69112b3fd05544cff2d1f873967a9cf6dab427a5
Dmitry Schegolihin <d.shegolihin@gmail.com>
2019-02-05 16:58:41 +0400

    Base components and pages for Posts/SinglePost.

---
+++ components/Posts.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="posts-list">
+    Posts list base template
+  </div>
+</template>
+
+<script>
+export const Posts = {
+  name: 'Posts',
+  methods: {
+    loadPostsList() {
+      this.$store.dispatch('wp/loadPostsList').then(res => {
+        success(res)
+      }).catch(err => {
+        failure(err)
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+  /* Always export scoped styles to not break the app */
+
+</style>
---
+++ components/SinglePost.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="single-post">
+    Single post base template
+  </div>
+</template>
+
+<script>
+export const SinglePost = {
+  name: 'SinglePost',
+  methods: {
+    loadPostData() {
+      this.$store.dispatch('wp/loadPost').then(res => {
+        success(res)
+      }).catch(err => {
+        failure(err)
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+  /* Always export scoped styles to not break the app */
+
+</style>
