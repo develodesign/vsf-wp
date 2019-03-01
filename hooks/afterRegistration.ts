82560022a5d61070ce4569cd5c3403cb9c76d160
Dmitry Schegolihin <d.shegolihin@gmail.com>
2019-03-01 09:59:05 +0400

    Upgrade hook signature to v1.8.0

--- hooks/afterRegistration.ts
+++ hooks/afterRegistration.ts
@@ -4,7 +4,7 @@ import SearchQuery from '@vue-storefront/core/lib/search/searchQuery'
 
 const EXTENSION_KEY = 'wordpress'
 
-export function afterRegistration (Vue, config, store, isServer) {
+export function afterRegistration ({ Vue, config, store, isServer} ) {
   if (!Vue.prototype.$isServer) {
     console.log('Register wordpress module')
   }
